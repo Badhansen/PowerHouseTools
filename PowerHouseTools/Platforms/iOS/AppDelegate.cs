b48904551104fca715dabb83db210499f7d8a834
Badhan Sen <badhan.cse@gmail.com>
2023-05-07 02:03:38 +0600

    Power house tools initial commit

---
+++ PowerHouseTools/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PowerHouseTools;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
